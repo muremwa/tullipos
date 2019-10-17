194a07ddb5c4d1fbb0668124fd6fa91d4e424ea5
Muremwa <danmburu254@gmail.com>
2019-10-17 16:08:53 +0300

    added an order page

--- bunker/views.py
+++ bunker/views.py
@@ -1,17 +1,27 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 from shop.models import CustomerOrder
 
 
 class AllOrders(View):
     def get(self, *args, **kwargs):
+        if not self.request.user.is_staff:
+            raise Http404
+
         orders = CustomerOrder.objects.order_by('-created')
         return render(self.request, 'bunker/all_orders.html', {
-            'orders': orders
+            'orders': orders,
         })
 
 
 class Order(View):
     def get(self, *args, **kwargs):
-        return render(self.request, 'bunker/order.html')
+        if not self.request.user.is_staff:
+            raise Http404
+
+        order = get_object_or_404(CustomerOrder, pk=kwargs['order_id'])
+        return render(self.request, 'bunker/order.html', {
+            'order': order,
+        })
